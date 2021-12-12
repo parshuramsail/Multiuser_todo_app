18d201279febedde0964f39d98bfb444b831eb56
Parshuramsail <parshusail416@gmail.com>
2021-12-12 22:27:16 +0530

    hosting

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web gunicorn Multiuser_todo_app.wsgi:application --log-file -
+web: gunicorn Multiuser_todo_app.wsgi
--- TODO_PROJECT/settings.py
+++ TODO_PROJECT/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,9 +124,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
-
-django_heroku.settings(locals)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
